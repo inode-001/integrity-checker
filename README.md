1b6a21798306b12ccdd69bc256bfe61c53706759
inode-001 <furyfish7@proton.me>
2024-04-03 06:03:22 -0500

    first commit of the file

---
+++ README.md
@@ -0,0 +1,1 @@
+This python scripts verifies if the integrity of a file by using a previous known checksum and checks if its the same as the file.
---
+++ verify.py
@@ -0,0 +1,16 @@
+import hashlib
+try:
+     fileName = input("Enter the file to check its integrity : ")
+
+     knownHash = str(input("Enter the known hash of the file : "))
+
+     with open(filename,'rb') as f:
+        bytes = f.read()
+        readableHash = hashlib.sha256(bytes).hexdigest()
+
+    if knownHash == readableHash:
+         print("Status:OK File Verified")
+    else:
+        print("Status:Not Okay File Isn't Legit")
+except FileNotFoundError:
+      print("Error:\tEnsure you input the right file path ")
