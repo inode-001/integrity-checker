b96fcf6def67f9e4bcf61f3d8ba2273957ebc7af
inode-001 <furyfish7@proton.me>
2024-04-20 02:34:59 -0500

    used PEP8-style

--- verify.py
+++ verify.py
@@ -1,16 +1,17 @@
 import hashlib
+
 try:
-     fileName = input("Enter the file to check its integrity : ")
+    fileName = input("Enter the file to check its integrity : ")
 
-     knownHash = str(input("Enter the known hash of the file : "))
+    knownHash = str(input("Enter the known hash of the file : "))
 
-     with open(fileName,'rb') as f:
+    with open(fileName, "rb") as f:
         bytes = f.read()
         readableHash = hashlib.sha256(bytes).hexdigest()
 
-     if knownHash == readableHash:
-          print("\n\t>>> Status:OK File Verified")
-     else:
+    if knownHash == readableHash:
+        print("\n\t>>> Status:OK File Verified")
+    else:
         print("Status:Not Okay File Isn't Legit")
 except FileNotFoundError:
-      print("Error:\tEnsure you input the right file path ")
+    print("Error:\tEnsure you input the right file path ")
